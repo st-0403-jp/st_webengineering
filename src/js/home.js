2fd25011662c97d5d38ee44b181d2a55a6c35189
st-0403-jp <sato252011@gmail.com>
2019-08-30 18:23:33 +0900

    [sp] モバイルフレンドリー対応

--- src/js/home.js
+++ src/js/home.js
@@ -61,37 +61,4 @@
         // });
     };
 
-    function slider () {
-        var $el = $('.js_slider_list');
-        var $paging = $('.js_paging');
-
-        function translate () {
-            var translate = $el.data('translate');
-            $el.css('transform', 'translateX(' + translate + 'vw)');
-        };
-
-        function prev () {
-            var translate = $el.data('translate');
-            translate = Number(translate) + 100;
-            $el.data('translate', translate + '');
-        };
-
-        function next () {
-            var translate = $el.data('translate');
-            translate = Number(translate) - 100;
-            $el.data('translate', translate + '');
-        };
-
-        translate();
-        $paging.on('click', '.prev', function () {
-            prev();
-            translate();
-        });
-        $paging.on('click', '.next', function () {
-            next();
-            translate();
-        });
-    };
-    slider();
-
 })();
